c15a6a58af5d6a6c3ecb39c1282e74b0cafff363
Slava Kirshman <slava.kirshman@icloud.com>
2019-05-21 08:47:45 +0300

    Mutations

---
+++ start/client/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+    },
+  }),
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages/>
+  </ApolloProvider>, document.getElementById('root'));
